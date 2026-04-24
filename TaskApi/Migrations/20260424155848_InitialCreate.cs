6b0665fbcaaaa910fcf60eeac9fb9a87baa3a9bd
James Reyes Calderon <jr76407900@gmail.com>
2026-04-24 10:03:07 -0600

    feat: Creamos la migracion inicialy actualizacion de todos los paquetes a V 9.0

---
+++ TaskApi/Migrations/20260424155848_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TaskApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Duedate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tareas");
+        }
+    }
+}
--- TaskApi/Models/Tarea.cs
+++ TaskApi/Models/Tarea.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;	
 
-namespace TaskApi.Models
+namespace TaskApi.Models;
 
     public class Tarea
 {
 
 	public int Id { get; set; }
 	[Required]
-        public string Title { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
 	public string Description { get; set; } = string.Empty;
 	public bool IsCompleted { get; set; } = false;
-        public DateTime? Duedate { get; set; }
+    public DateTime? Duedate { get; set; }
     
 }	
