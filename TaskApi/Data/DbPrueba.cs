14205e4bd6b9900c030a48a20ea9f01b681b9e8c
James Reyes Calderon <jr76407900@gmail.com>
2026-04-24 09:42:32 -0600

    feat: Implementacion del Models Tarea, Configuracion de Base datos y el Program

---
+++ TaskApi/Data/DbPrueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TaskApi.Models;
+
+namespace TaskApi.Data;
+
+    public class DbPrueba: DbContext
+{
+	public DbPrueba(DbContextOptions <DbPrueba> options): base(options) {	}
+
+	public DbSet<Tarea> Tareas { get; set; }
+}
---
+++ TaskApi/Models/Tarea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;	
+
+namespace TaskApi.Models
+
+    public class Tarea
+{
+
+	public int Id { get; set; }
+	[Required]
+        public string Title { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
+	public bool IsCompleted { get; set; } = false;
+        public DateTime? Duedate { get; set; }
+    
+}	
