7088f7fda5a9f8d164293eeb4675a18a61e64b1e
James Reyes Calderon <jr76407900@gmail.com>
2026-04-26 10:53:17 -0600

    feat: Instalamos el proyecto de angular y se creo la estructura del frontend

---
+++ Frontend/PruebaCliente/src/app/components/tarea/tarea.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea',
+  imports: [],
+  templateUrl: './tarea.html',
+  styleUrl: './tarea.css',
+})
+export class Tarea {
+
+}
